d9d04057903a02e5c815c52a2133790292296dab
Ramizi1 <120492343+Ramizi1@users.noreply.github.com>
2025-12-28 08:25:47 -0500

    Update r53.tf

--- resume_challenge/terraform/modules/r53.tf
+++ resume_challenge/terraform/modules/r53.tf
@@ -9,9 +9,3 @@ output "acm_validation" {
   value       = aws_acm_certificate.acm_cert.domain_validation_options
   description = "Add these TXT records in Namecheap to validate ACM certificate"
 }
-
-viewer_certificate {
-  acm_certificate_arn = aws_acm_certificate.ssl_cert.arn
-  ssl_support_method = "sni-only"
-  minimum_protocol_version = "TLSv1.2_2019"
-}
