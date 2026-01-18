134e877882a33ecca63ad4b4ca0d6fbbfedce36d
Ramiz Shefkiu <ramizshefkiu2004@gmail.com>
2026-01-18 09:58:51 -0500

    updating modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,16 @@ module "frontend_s3" {
   source = "./modules/s3"
 }
 
-module "cdn" {
-  source = "./modules/cloudfront"
-}
-
 module "dns" {
   source = "./modules/route53"
+}
+
+module "cdn" {
+  source = "./modules/cloudfront"
+
+  bucket_id     = module.frontend_s3.bucket_id
+  bucket_domain = module.frontend_s3.bucket_domain
+  acm_cert_arn  = module.dns.acm_cert_arn
 }
 
 module "backend" {
